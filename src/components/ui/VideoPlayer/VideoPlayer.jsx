193de474c544e13b446675e09cf3ad1cc706d93c
IceLink1 <163158076+IceLink1@users.noreply.github.com>
2024-08-08 10:09:15 +0500

    Update VideoPlayer.jsx

--- src/components/ui/VideoPlayer/VideoPlayer.jsx
+++ src/components/ui/VideoPlayer/VideoPlayer.jsx
@@ -9,8 +9,7 @@ export default function VideoPlayer() {
   useEffect(() => {
     const dataUrl = window.location.href;
     fetch(
-      '//pleer.videoplayers.club/get_player?w=610&h=370&type=widget&kp_id=&players=videocdn,hdvb,bazon,alloha,ustore,kodik,trailer,torrent&r_id=videoplayers&vni=VIDEOCDN&vti=&vdi=&hni=HDVB&hti=&hdi=&bni=BAZON&bti=&bdi=&alni=ALLOHATV&alti=&aldi=&usni=USTOREBZ&usti=&usdi=&koni=KODIK&koti=&kodi=&tti=&ru=' +
-        dataUrl,
+      '//pleer.videoplayers.club/get_player?w=610&h=370&type=widget&kp_id=&players=videocdn,hdvb,bazon,alloha,ustore,kodik,trailer,torrent&r_id=videoplayers&vni=VIDEOCDN&vti=&vdi=&hni=HDVB&hti=&hdi=&bni=BAZON&bti=&bdi=&alni=ALLOHATV&alti=&aldi=&usni=USTOREBZ&usti=&usdi=&koni=KODIK&koti=&kodi=&tti=&ru='+dataUrl,
     ).then(res => res.text()).then(data => setScriptHtml(data.match(/<iframe.*<\/iframe>/gm)[1]));
   }, []);
 
